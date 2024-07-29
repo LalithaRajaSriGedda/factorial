125962aed27a856bb8859141a5051212a6a87df9
Lalitha Raja Sri Gedda <lalithagedda99@gmail.com>
2024-07-29 14:53:34 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -12,7 +12,7 @@ public class Factorial {
 
     public static void main(String[] args) {
         int n=5,i,fact=1;
-        for(i=0;i<=n;i++){
+        for(i=1;i<=n;i++){
             fact=fact*i;
         }
     
